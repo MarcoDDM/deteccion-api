dc37034e23db9be00dbc2aa964c700124a3243d7
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2019-10-17 16:56:43 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# deteccion-api
---
+++ server.js
@@ -0,0 +1,108 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors())
+
+const database = {
+	users: [
+		{
+			id: '123',
+			name: 'John',
+			email: 'john@gmail.com',
+			password: 'cookies',
+			entries: 0,
+			joined: new Date()
+		},
+		{
+			id: '124',
+			name: 'Maria',
+			email: 'maria@gmail.com',
+			password: 'bananas',
+			entries: 0,
+			joined: new Date()
+		}
+	],
+	login: [
+		{
+			id:'987',
+			has: '',
+			email: 'john@gmail.com'
+		}
+	]
+}
+
+app.get('/', (req, res)=> {
+	res.send(database.users);
+
+})
+
+app.post('/signin', (req, res) => {
+bcrypt.compare("apples", "$2a$10$yo.wO/QGy3GKTM7YFC2RceE2.YwFyuC06efcxCwRQ9ghuUdO/QFKe", function(err, res) {
+    console.log('first guess', res)
+});
+bcrypt.compare("veggies", "$2a$10$yo.wO/QGy3GKTM7YFC2RceE2.YwFyuC06efcxCwRQ9ghuUdO/QFKe", function(err, res) {
+    console.log('second guess', res)
+});	
+  if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
+  	res.json(database.users[0]);
+  } else {
+  	res.status(400).json('error logging in');
+  }
+})
+
+app.post('/register', (req,res) => {
+	const { email, name, password} = req.body;
+	
+	database.users.push({
+			id: '125',
+			name: name,
+			email: email,
+			password: password,
+			entries: 0,
+			joined: new Date()
+		})
+	res.json(database.users[database.users.length-1]);
+})
+
+app.get('/profile/:id', (req, res) => {
+	const { id } = req.params;
+	let found = false;
+	database.users.forEach(user => {
+		if (user.id === id) {
+			found = true;
+			return res.json(user);
+		} 
+	})
+	if (!found) {
+		res.status(400).json('not found');
+	}
+})
+
+app.put('/image', (req, res) => {
+	const { id } = req.body;
+	let found = false;
+	database.users.forEach(user => {
+		if (user.id === id) {
+			found = true;
+			user.entries++
+			return res.json(user.entries);
+		} 
+	})
+	if (!found) {
+		res.status(400).json('not found');
+	}
+})
+
+
+
+
+
+app.listen(3000, () => {
+	console.log('App is running on port 3000')
+})
+
