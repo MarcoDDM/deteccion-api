c529b126d340c88e3abb9f1733317f9fc55b111a
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2019-10-20 15:39:56 -0300

    Cambio en llamado de funciones

--- server.js
+++ server.js
@@ -2,101 +2,47 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex')
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'danndee89',
+    database : 'deteccion'
+  }
+});
+
+db.select('*').from('users');
+
+/*db.select('*').from('users').then(data => {
+	console.log('esto era data');
+});
+*/
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors())
 
-const database = {
-	users: [
-		{
-			id: '123',
-			name: 'John',
-			email: 'john@gmail.com',
-			password: 'cookies',
-			entries: 0,
-			joined: new Date()
-		},
-		{
-			id: '124',
-			name: 'Maria',
-			email: 'maria@gmail.com',
-			password: 'bananas',
-			entries: 0,
-			joined: new Date()
-		}
-	],
-	login: [
-		{
-			id:'987',
-			has: '',
-			email: 'john@gmail.com'
-		}
-	]
-}
 
 app.get('/', (req, res)=> {
 	res.send(database.users);
-
 })
 
-app.post('/signin', (req, res) => {
-bcrypt.compare("apples", "$2a$10$yo.wO/QGy3GKTM7YFC2RceE2.YwFyuC06efcxCwRQ9ghuUdO/QFKe", function(err, res) {
-    console.log('first guess', res)
-});
-bcrypt.compare("veggies", "$2a$10$yo.wO/QGy3GKTM7YFC2RceE2.YwFyuC06efcxCwRQ9ghuUdO/QFKe", function(err, res) {
-    console.log('second guess', res)
-});	
-  if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-  	res.json(database.users[0]);
-  } else {
-  	res.status(400).json('error logging in');
-  }
-})
+app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)})
 
-app.post('/register', (req,res) => {
-	const { email, name, password} = req.body;
-	
-	database.users.push({
-			id: '125',
-			name: name,
-			email: email,
-			password: password,
-			entries: 0,
-			joined: new Date()
-		})
-	res.json(database.users[database.users.length-1]);
-})
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 
-app.get('/profile/:id', (req, res) => {
-	const { id } = req.params;
-	let found = false;
-	database.users.forEach(user => {
-		if (user.id === id) {
-			found = true;
-			return res.json(user);
-		} 
-	})
-	if (!found) {
-		res.status(400).json('not found');
-	}
-})
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
 
-app.put('/image', (req, res) => {
-	const { id } = req.body;
-	let found = false;
-	database.users.forEach(user => {
-		if (user.id === id) {
-			found = true;
-			user.entries++
-			return res.json(user.entries);
-		} 
-	})
-	if (!found) {
-		res.status(400).json('not found');
-	}
-})
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 
 
 
